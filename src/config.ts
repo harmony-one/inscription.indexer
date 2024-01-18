18b7596d01fbc9597008b45ab1dce10e0c94399b
potvik <yuriy@harmony.one>
2024-01-18 22:04:55 +0300

    Fix internal service port

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ import * as process from 'process';
 export default () => ({
   version: process.env.npm_package_version || '0.0.1',
   name: process.env.npm_package_name || '',
-  port: parseInt(process.env.PORT, 10) || 8080,
+  port: parseInt(process.env.PORT, 10) || 3001,
 });
