c8f843683d067ad89ec51d90a135f3a09e2928de
artemkolodko <temakolodko@gmail.com>
2024-01-24 17:30:23 +0000

    Add certificate generation

--- src/lottery/lottery.service.ts
+++ src/lottery/lottery.service.ts
@@ -22,7 +22,7 @@ export class LotteryService {
   public start() {
     this.syncLottery();
     // this.registerDomain(
-    //   'anylongname1234567',
+    //   'anylongname12345678',
     //   '0x95D02e967Dd2D2B1839347e0B84E59136b11A073',
     // );
   }
@@ -83,32 +83,39 @@ export class LotteryService {
       domainName,
       ownerAddress,
     );
-    console.log('Register domain web3 tx hash:', transactionHash);
+    this.logger.log(`Register domain web3 tx hash: ${transactionHash}`);
 
     await new Promise((resolve) => setTimeout(resolve, 5000));
 
     try {
       this.logger.log(
-        'Starting register WEB2 domain...',
-        domainName,
-        ownerAddress,
-        transactionHash,
+        `Start registering web2 domain... ${domainName} ${ownerAddress} ${transactionHash}`,
       );
       const result = await this.registerDomainRelayer(
         domainName,
         ownerAddress,
         transactionHash,
       );
+      this.logger.log(`Web2 domain registered!`);
     } catch (e) {
       this.logger.error('Register WEB2 domain failed:', e.message);
     }
 
     try {
-      this.logger.log('Starting generate NFT...');
+      this.logger.log('Start generating NFT...');
       const result = await this.generateNFT(domainName);
       this.logger.log(`NFT generated:`, result);
     } catch (e) {
       this.logger.error('Generate NFT error:', e.message);
+    }
+
+    try {
+      this.logger.log('Starting generating cert...');
+      const result = await this.createCert(domainName, ownerAddress);
+      this.logger.log(`Cert generated:`, result);
+      this.logger.log(`Check domain: https://${result.sld}.country`);
+    } catch (e) {
+      this.logger.error('Generate cert error:', e.message);
     }
   }
 
@@ -156,5 +163,22 @@ export class LotteryService {
       domain: `${domainName}.country`,
     });
     return data as { generated: boolean; metadata: any; };
+  }
+
+  private async createCert(domainName: string, address: string, async = true) {
+    const {
+      data: { success, sld, mcJobId, nakedJobId, error },
+    } = await axios.post(`${this.relayerURL}/cert`, {
+      domain: `${domainName}.country`,
+      address,
+      async,
+    });
+    return {
+      success,
+      sld,
+      mcJobId,
+      nakedJobId,
+      error,
+    };
   }
 }
